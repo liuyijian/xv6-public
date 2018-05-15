bc642050c828c2d796d35d08eda0d577555ff6f5
zzh428 <624777990@qq.com>
2018-05-15 23:29:17 +0800

    add timestamptodate

--- find.c
+++ find.c
@@ -1,7 +1,15 @@
+/*
+*文件功能：文件查找
+*历史来源：四字班方案一代码
+*整合作者：赵哲晖
+*整合时间：2018/05/09
+*/
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "fs.h"
+#include "date.h"
 
 char* getFnameFromPath(char* path,char* fname)
 {
@@ -128,6 +136,90 @@ ls(char *path)
 	close(fd);
 }
 
+/*
+*函数功能：uint转化为实时时钟
+*作者：赵哲晖
+*时间：2018/05/15
+*/
+rtcdate* timestampToDate(uint stamp)
+{
+  uint ret = 946684800; //utc+0 2000/1/1 0:0:0 946684800
+  uint YEAR_SEC = 31536000;
+  uint DAY_SEC = 86400;
+  stamp -= ret;
+  int years = 0;
+  //get years
+  while(1)
+  {
+    if(years != 0 && years % 4 == 0)
+    {
+      if(stamp >= YEAR_SEC + DAY_SEC)
+      {
+        stamp = stamp - (YEAR_SEC + DAY_SEC);
+        years++;
+      }
+      else
+        break;
+    }
+    else
+    {
+      if(stamp >= YEAR_SEC)
+      {
+        stamp -= YEAR_SEC;
+        years++;
+      }
+      else
+        break;
+    }
+  }
+  //get month
+  int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+  if(years != 0 && years % 4 == 0)
+    days[1] = 29;
+  int month = 1;
+  while(1)
+  {
+    int temp = stamp - days[month-1] * DAY_SEC;
+    if(temp >= 0)
+    {
+      stamp = temp;
+      month++;
+    }  
+    else
+      break;
+  }
+  //get days
+  int day = 1;
+  while(stamp >= DAY_SEC)
+  {
+    stamp -= DAY_SEC;
+    day++;
+  }
+  //get hours
+  int hour = 0;
+  while(stamp >= 3600)
+  {
+    stamp -= 3600;
+    hour++;
+  }
+  //get minutes
+  int minute = 0;
+  while(stamp >= 60)
+  {
+    stamp -= 60;
+    minute++;
+  }
+  int second = stamp;
+  rtcdate * date = malloc(sizeof(rtcdate));
+  date->year = years + 2000;
+  date->month = month;
+  date->day = day;
+  date->hour = hour;
+  date->minute = minute;
+  date->second = second;
+  return date;
+}
+
 void sf(char *path, char *fname)
 {
 	char buf[512], *p;
@@ -153,7 +245,9 @@ void sf(char *path, char *fname)
 			char name[100];
 			if(compare(getFnameFromPath(path,name),fname) == 0)
 			{
-				printf(1, "%s path:%s  size:%d  time:%d\n", fname, path,st.size,st.ctime);
+				rtcdate * date = timestampToDate(st.ctime);
+				printf(1, "%s path:%s  size:%d  time: %d/%d/%d %d:%d:%d  %d\n", fname, path,st.size,date->year,date->month,date->day,date->hour,date->minute,date->second,st.ctime);
+				free(date);
 			}
 			//printf(1,"%s %d %d", getFnameFromPath(path,name), compare(getFnameFromPath(path,name),"."), compare(getFnameFromPath(path,name),".."));
 			//printf(0,"1/n");
@@ -187,7 +281,9 @@ void sf(char *path, char *fname)
 			char name[100];
 			if(compare(getFnameFromPath(buf,name),fname) == 0)
 			{
-				printf(1,"%s path:%s  size:%d  time:%d\n", fname, buf, st.size,st.ctime);
+				rtcdate * date = timestampToDate(st.ctime);
+				printf(1, "%s path:%s  size:%d  time: %d/%d/%d %d:%d:%d  %d\n", fname, path,st.size,date->year,date->month,date->day,date->hour,date->minute,date->second,st.ctime);
+				free(date);
 			}
 			if((st.type == 1) && (compare(getFnameFromPath(buf,name),".") != 0) && (compare(getFnameFromPath(buf,name),"..") != 0))
 			{
